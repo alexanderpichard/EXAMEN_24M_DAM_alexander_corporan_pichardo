c57217e469f27f1c8b11dbe052b0c14f9c0f6cc3
Alexander Pichard <39698441+alexanderpichard@users.noreply.github.com>
2021-03-24 11:49:07 +0100

    esta es la rama1

--- src/Examen.java
+++ src/Examen.java
@@ -2,7 +2,15 @@
 public class Examen {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num =0;
 		System.out.println("buenos dias ceinmark, soy marta gomez "
 				+ "Introduce un numero para averiguar si es primo o no: ");
+				num=Integer.parseInt(sc.nextLine());
+				if (%num==0) {
+					System.out.println(num + "es un nuemero par");
+				}else {
+					System.out.println("es impar ");
+				}
 	}
 }
